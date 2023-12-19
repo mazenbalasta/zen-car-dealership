bc9cfc5a2a390e9f71b07aa9ab4ded59222d2991
Jeremy <jeremykilcrease@yahoo.com>
2023-12-19 17:38:19 -0600

    Add ListCustomers component and comment out unused code

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ListCustomers from './Sales/ListCustomers';
 import CustomerForm from './Sales/CustomerForm';
 import RecordSaleForm from './Sales/RecordSaleForm';
 import ListSalesPeople from './Sales/ListSalesPeople';
 import ListAllSales from './Sales/ListAllSales';
-import ListCustomers from './Sales/ListCustomers';
 import SalesPersonForm from './Sales/SalesPersonForm';
-// import ManufacturersList from './Inventory/ManufacturesList';
 import AutomobileList from './Inventory/AutomobileList';
-// import ModelsList from './Inventory/ModelList';
 import TechnicianList from './Service/TechnicianList';
 import NewTechnicianForm from './Service/NewTechnicianForm';
 import NewServiceAppointmentForm from './Service/NewServiceForm';
 import ServiceAppointmentList from './Service/ServiceList';
 import AutomobileForm from './Inventory/CreateAutomobileForm';
 import ServiceHistory from './Service/ServiceHistory';
+// import ManufacturersList from './Inventory/ManufacturesList';
 // import ManufacturerForm from './Inventory/CreateManufacturerForm';
+// import ModelsList from './Inventory/ModelList';
 // import VehicleModelForm from './Inventory/CreateVehicleModelForm';
 
 function App() {
@@ -33,21 +33,18 @@ function App() {
           <Route path="salespeople/" element={<SalesPersonForm />} />
           <Route path="sales/" element={<RecordSaleForm />} />
           <Route path="sales/history/" element={<ListAllSales />} />
-          {/* <Route path="models/create/" element={<VehicleModelForm />} /> */}
-          {/* <Route path="manufacturers/create/" element={<ManufacturerForm />} /> */}
-          <Route path="automobiles/create/" element={<AutomobileForm />} />
-          {/* <Route path="models/create/" element={<VehicleModelForm />} />
-          <Route path="manufacturers/create/" element={<ManufacturerForm />} />
-          <Route path="automobiles/create/" element={<AutomobileForm />} /> */}
           <Route path="/technicians" element={<TechnicianList />} />
           <Route path="/technicians/new" element={<NewTechnicianForm />} />
           <Route path="/appointments" element={<ServiceAppointmentList />} />
           <Route path="/appointments/new" element={<NewServiceAppointmentForm />} />
-          {/* <Route path="manufacturers/list/" element={<ManufacturersList />}/> */}
           <Route path="/appointments/history" element={<ServiceHistory />} />
-          {/* <Route path="manufacturers/list/" element={<ManufacturersList />}/>
           <Route path="automobiles/list/" element={<AutomobileList />}/>
+          <Route path="automobiles/create/" element={<AutomobileForm />} />
+          {/* <Route path="manufacturers/list/" element={<ManufacturersList />}/> */}
+          {/* <Route path="manufacturers/create/" element={<ManufacturerForm />} /> */}
           {/* <Route path="models/list/" element={<ModelsList />}/> */}
+          {/* <Route path="models/create/" element={<VehicleModelForm />} /> */}
+
         </Routes>
       </div>
     </BrowserRouter>
