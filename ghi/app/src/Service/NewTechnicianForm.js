cd6a946e29d6a496cfa80d6afd47f0325939d73a
Mazen Balasta <mzn_300@yahoo.com>
2023-12-20 23:48:46 +0800

    error handling on NewTechnicianForm

--- ghi/app/src/Service/NewTechnicianForm.js
+++ ghi/app/src/Service/NewTechnicianForm.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 
@@ -22,55 +22,57 @@ function NewTechnicianForm () {
     });
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const confirmSubmit = window.confirm('Are you sure you want to create this new technician?');
 
+    if (confirmSubmit) {
+      const url = `http://localhost:8080/api/technicians/`;
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const confirmSubmit = window.confirm('Are you sure you want to create this new technician?');
+      const fetchConfig = {
+        method: 'post',
+        body: JSON.stringify(formData),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+      };
 
-        if (confirmSubmit) {
-          const url = `http://localhost:8080/api/technicians/`;
-
-          const fetchConfig = {
-              method: 'post',
-              body: JSON.stringify(formData),
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-          };
-
-          const response = await fetch (url, fetchConfig);
-          if (response.ok) {
-            setFormData({ ...InitForm })
-
-          }
-        } else {window.alert('Creation cancelled')}
+      const response = await fetch (url, fetchConfig);
+      if (response.ok) {
+        setFormData({ ...InitForm })
+        navigate("/technicians")
+        window.alert('Technician created!');
+      }
+    } else {
+      window.alert('Creation cancelled');
+      setFormData({ ...InitForm })
     }
+  }
 
-    return (
-        <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
-            <h1>Add a Technician</h1>
-            <form onSubmit={handleSubmit} id="aff-shoe-form">
-              <div className="form-floating mb-3">
-                <input onChange={handleInputChange} value={formData.first_name} placeholder="First Name" required type="text" name="first_name" id="first_name" className="form-control" />
-                <label htmlFor="first_name">First Name</label>
-              </div>
-              <div className="form-floating mb-3">
-                <input onChange={handleInputChange} value={formData.last_name} placeholder="Last Name" required type="text" name="last_name" id="last_name" className="form-control" />
-                <label htmlFor="last_name">Last Name</label>
-              </div>
-              <div className="form-floating mb-3">
-                <input onChange={handleInputChange} value={formData.employee_id} placeholder="Employee ID" required type="text" name="employee_id" id="employee_id" className="form-control" />
-                <label htmlFor="employee_id">Employee ID</label>
-              </div>
-              <button className="btn btn-primary">Create</button>
-            </form>
-          </div>
+  return (
+    <div className="row">
+      <div className="offset-3 col-6">
+        <div className="shadow p-4 mt-4">
+          <h1>Add a Technician</h1>
+          <form onSubmit={handleSubmit} id="aff-shoe-form">
+            <div className="form-floating mb-3">
+              <input onChange={handleInputChange} value={formData.first_name} placeholder="First Name" required type="text" name="first_name" id="first_name" className="form-control" />
+              <label htmlFor="first_name">First Name</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input onChange={handleInputChange} value={formData.last_name} placeholder="Last Name" required type="text" name="last_name" id="last_name" className="form-control" />
+              <label htmlFor="last_name">Last Name</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input onChange={handleInputChange} value={formData.employee_id} placeholder="Employee ID" required type="text" name="employee_id" id="employee_id" className="form-control" />
+              <label htmlFor="employee_id">Employee ID</label>
+            </div>
+            <button className="btn btn-primary">Create</button>
+          </form>
         </div>
       </div>
+    </div>
   )
-}
+};
 
 export default NewTechnicianForm
